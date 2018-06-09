896ff5c53ecf43f86981e5e9e0a8712923e80bdd
Joao Francisco Goncalves Sobrinho Junior <jfjunior10@gmail.com>
2018-06-09 11:47:08 -0300

    configure test

---
+++ src/app/form/form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormComponent } from './pages/form/form.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FormComponent]
+})
+export class FormModule { }
---
+++ src/app/list/list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './pages/list/list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ListComponent]
+})
+export class ListModule { }
